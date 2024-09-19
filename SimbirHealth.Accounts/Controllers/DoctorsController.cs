02f1aeb201648db5f8c20fb0d9d30d2f3d626018
SiVittorio <vittorio.kru@yandex.ru>
2024-09-19 18:49:17 +0300

    Добавьте файлы проекта.

---
+++ SimbirHealth.Accounts/Controllers/DoctorsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimbirHealth.Accounts.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorsController : ControllerBase
+    {
+        private readonly ILogger<DoctorsController> _logger;
+
+        public DoctorsController(ILogger<DoctorsController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ SimbirHealth.Common/SimbirHealthContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SimbirHealth.Common
+{
+    public class SimbirHealthContext : DbContext
+    {
+        public SimbirHealthContext(DbContextOptions<SimbirHealthContext> options) : base(options)
+        {
+            Database.EnsureCreated(); // create if not exist
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // TODO: Добавить модели по умолчанию OnModelCreating()
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
